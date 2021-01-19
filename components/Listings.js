2bcae50dff8e23124c8d7bd89dd0f6c1b11642a8
Emma Aia <emmaaia@192-168-1-109.tpgi.com.au>
2021-01-19 14:30:38 +1100

    listed cars

--- components/Listings.js
+++ components/Listings.js
@@ -1,28 +1,103 @@
+import { useState, useEffect } from "react";
+import { CLEAR_LOGGEDIN_FBUSER } from "../../../henry-school-frontend/src/types";
+
 const ACTIONS = ["View", "Reply"];
 
-const formatAsCurrency = (int) => {
-    // TODO
-};
+const formatAsCurrency = (int) =>
+  new Intl.NumberFormat("en-AU", {
+    style: "currency",
+    currency: "AUD",
+  }).format(int);
 
-const Listing = (props) => {
-    // TODO
-    // This should be the component which renders an individual listing to the page
-
-    return <div className="listing"></div>;
-};
-
-const Listings = (props) => {
-    // TODO
-    // This component should make a request to the api endpoint (props.dataEndpoint)
-    // then render the result as set of listings as per the design mocks
-    // check props passed in from parent for other values that you may need to use
-
-    return (
-        <div>
-            <div className="listings__header"></div>
-            <div className="listings__grid"></div>
+const Listing = (srz) =>
+  srz.length === 0 ? (
+    <p>No item.</p>
+  ) : (
+    srz.map((el, idx) => {
+      return (
+        <div key={el.title + String(idx)} className="listing__card">
+          <h3 className="oneline-truncated">{el.title}</h3>
+          <p className="price-location">
+            <span className="color-primary">
+              <b>
+                {Number.isInteger(el.price)
+                  ? formatAsCurrency(el.price)
+                  : el.price}
+              </b>
+            </span>
+            <span className="color-text-light">{el.location}</span>
+          </p>
+          {el.imgUrl && (
+            <div className="image__container">
+              <img
+                className="img-adjustemnt"
+                src={el.imgUrl}
+                alt={`${el.title}_${idx}`}
+              />
+            </div>
+          )}
+          <p className="threelines-truncated">{el.description}</p>
+          <div>
+            {ACTIONS.map((buttonText, idx) => (
+              <button
+                key={`${buttonText}__${idx}`}
+                type="button"
+                className="card__button"
+                onClick={() => {
+                  console.log(`${buttonText}: ${el.title}`);
+                }}
+              >
+                {buttonText}
+              </button>
+            ))}
+          </div>
         </div>
-    );
+      );
+    })
+  );
+
+const Listings = ({ dataEndpoint, keyword, location }) => {
+  // TODO
+  // This component should make a request to the api endpoint (props.dataEndpoint)
+  // then render the result as set of listings as per the design mocks
+  // check props passed in from parent for other values that you may need to use
+  const [searchResultes, setSearchResultes] = useState([]);
+
+  useEffect(() => {
+    const fetchRz = async () => {
+      try {
+        const results = await (
+          await fetch(`http://localhost:3000${dataEndpoint}`)
+        ).json();
+
+        if (!results) {
+          console.log("31 -- please check your data return error...");
+        } else {
+          setSearchResultes(results);
+        }
+      } catch (error) {
+        console.log("37 -- error: ", error.message);
+      }
+    };
+    (!searchResultes || searchResultes.length === 0) && fetchRz();
+    //   return () => {}
+  });
+
+  if (!searchResultes)
+    <div>Usually, place shared spinner component here...</div>;
+  return (
+    <div>
+      <div className="listings__header">
+        <h2 className="mb-0 color-text-dark">Search Result</h2>
+        <p className="mt-0 color-text-light">
+          <span className="color-primary">{6} results</span> for{" "}
+          <span className="color-primary">{keyword}</span> in{" "}
+          <span className="color-primary">{location}</span>
+        </p>
+      </div>
+      <div className="listings__grid">{Listing(searchResultes)}</div>
+    </div>
+  );
 };
 
 export default Listings;
